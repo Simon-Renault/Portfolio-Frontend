66956980775a021a0166e354a5874b361077979e
Simon-Renault <simon.renault.pro@gmail.com>
2019-05-28 21:11:03 +0200

    new build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5f76a70643f428aede181917c865e61.js"
+  "/precache-manifest.2baad5a8e4a55abeafeb7753e86e05c5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio-prod"});
